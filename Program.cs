c05b2721a7f065451c73581bd2431f10f851d886
Ovidiu <ovidiu.sbangu@gmail.com>
2024-02-01 13:12:25 +0200

    add new webapp

--- Program.cs
+++ Program.cs
@@ -1,31 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
 
-class Program
-{
-    static void Main()
-    {
-        const int port = 5000;
+app.MapGet("/", () => "Hello World!");
 
-        TcpListener listener = new TcpListener(IPAddress.Any, port);
-        listener.Start();
-        Console.WriteLine($"Listening on port {port}...");
-
-        while (true)
-        {
-            using (TcpClient client = listener.AcceptTcpClient())
-            {
-                Console.WriteLine("Client connected");
-
-                using (NetworkStream stream = client.GetStream())
-                {
-                    byte[] data = Encoding.UTF8.GetBytes("Hello, docker!\n");
-                    stream.Write(data, 0, data.Length);
-                }
-            }
-        }
-    }
-}
+app.Run();
